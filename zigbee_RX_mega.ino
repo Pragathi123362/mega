c54676d612acfca7553730b20b4d23a9c1786cdd
Pragathi123362 <75770397+Pragathi123362@users.noreply.github.com>
2021-04-10 14:39:49 +0530

    Add files via upload

---
+++ zigbee_RX_mega.ino
@@ -0,0 +1,17 @@
+char b;
+ void setup()
+{
+Serial.begin(9600);
+}
+void loop()
+{
+while (Serial.available()>0) {
+  b=Serial.read();
+Serial.println(b); 
+
+}
+}
+
+//here uno acts as tx part and mega acts as rx part. so connect arduino tx to zigbee rx as transmitting side
+
+
